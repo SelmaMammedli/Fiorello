f3b42d76da57b36b3bc0ff7ab8993ea869f2bff2
SelmaMammedli <selma.mammedli@gmail.com>
2023-12-26 23:33:09 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fiorello.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
