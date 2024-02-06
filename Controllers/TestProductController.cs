ba3f467e977ec999b80362cd33fc989d03eeb168
SelmaMammedli <selma.mammedli@gmail.com>
2024-02-06 21:50:59 +0400

    view component

---
+++ Controllers/TestProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.Controllers
+{
+    public class TestProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
