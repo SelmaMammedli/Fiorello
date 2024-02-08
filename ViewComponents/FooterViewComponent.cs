0fbe866c9cd1beb6b81b66bdd64a0a617614e2e6
SelmaMammedli <selma.mammedli@gmail.com>
2024-02-08 22:44:15 +0400

    feat:footer view component

---
+++ ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.ViewComponents
+{
+    public class FooterViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var bios = _context.Bios
+                .ToDictionary(b => b.Key, b => b.Value);
+
+
+
+
+            return View(await Task.FromResult(bios));
+
+        }
+    }
+}
