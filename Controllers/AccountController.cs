2c34cd23b0bd7ab7b9abb846927601127da65ade
SelmaMammedli <selma.mammedli@gmail.com>
2024-03-07 16:02:17 +0400

    register completed

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiorello.Models;
+using Fiorello.ViewModels.UserVM;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiorello.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+
+            if(!ModelState.IsValid) return View();
+            AppUser user = new();
+            user.FullName = registerVM.FullName;
+            user.Email = registerVM.Email;
+            user.UserName = registerVM.UserName;
+            IdentityResult result= await _userManager.CreateAsync(user,registerVM.Password);
+            if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(registerVM);
+
+            }
+            return RedirectToAction("Index","Home");
+
         }
     }
 }
---
+++ ViewModels/UserVM/RegisterVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fiorello.ViewModels.UserVM
+{
+    public class RegisterVM
+    {
+        [System.ComponentModel.DataAnnotations.Required, MaxLength(100)]
+        public string FullName { get; set; }
+        [System.ComponentModel.DataAnnotations.Required, MaxLength(100)]
+        public string UserName { get; set; }
+        [System.ComponentModel.DataAnnotations.Required, EmailAddress,DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [System.ComponentModel.DataAnnotations.Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [System.ComponentModel.DataAnnotations.Required, DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
