1a3319fd982d44e72845dcf12d0e9c4e09f1a136
SelmaMammedli <selma.mammedli@gmail.com>
2024-03-12 00:39:28 +0400

    role create and delete completed

--- Areas/AdminArea/Controllers/RoleController.cs
+++ Areas/AdminArea/Controllers/RoleController.cs
@@ -16,5 +16,20 @@ namespace Fiorello.Areas.AdminArea.Controllers
         {
             return View(_roleManager.Roles.ToList());
         }
+        public async Task<IActionResult> Create(string role)
+        {
+            if(string.IsNullOrEmpty(role)) return BadRequest("input bos ola bilmez");
+            var result=await _roleManager.CreateAsync(new IdentityRole { Name= role });
+            if (!result.Succeeded) BadRequest("input bos ola bilmez");
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role=await _roleManager.FindByIdAsync(id);
+            if (role is null) return NotFound();
+            await _roleManager.DeleteAsync(role);
+            return RedirectToAction("Index");
+            
+        }
     }
 }
