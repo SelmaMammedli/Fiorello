c78a9316bf3f58a67238b8918beea051d1983a43
SelmaMammedli <selma.mammedli@gmail.com>
2024-03-15 22:42:47 +0400

    cookie change

--- Areas/AdminArea/Controllers/BasketController.cs
+++ Areas/AdminArea/Controllers/BasketController.cs
@@ -8,5 +8,15 @@ namespace Fiorello.Areas.AdminArea.Controllers
         {
             return View();
         }
+        public IActionResult SetData()
+        {
+            HttpContext.Response.Cookies.Append("name","filankes",new CookieOptions { MaxAge=TimeSpan.FromMinutes(10)});
+            return Content("Set olundu");
+        }
+        public IActionResult GetData()
+        {
+            var data = Request.Cookies["name"];
+            return Content(data); 
+        }
     }
 }
