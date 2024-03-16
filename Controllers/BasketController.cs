cf769c5aeb6bddf3aeb488ccb99771105e2a3119
SelmaMammedli <selma.mammedli@gmail.com>
2024-03-17 00:07:01 +0400

    add basket increase decrease remove completed

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -74,5 +74,105 @@ namespace Fiorello.Controllers
 
             return View(list);
         }
+        public IActionResult Increase(int?id)
+        {
+            if (id == null) return BadRequest();
+            var product = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.ProductImages)
+                .FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            List<BasketProductVM> list=new();
+            var basket = Request.Cookies["basket"];
+           
+                list = JsonConvert.DeserializeObject<List<BasketProductVM>>(basket);
+            
+            var existProductInBasket = list.Find(p => p.Id == id);
+            if (existProductInBasket is null)
+            {
+                BasketProductVM basketProductVM = new()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    CategoryName = product.Category.Name,
+                    MainImageUrl = product.ProductImages.Any(p => p.IsMain) ?
+                     product.ProductImages.FirstOrDefault(p => p.IsMain).ImageUrl :
+                     product.ProductImages.FirstOrDefault().ImageUrl,
+                    BasketCount = 1
+                };
+                list.Add(basketProductVM);
+            }
+            else
+            {
+                existProductInBasket.BasketCount++;
+            }
+
+            var stringProduct = JsonConvert.SerializeObject(list);
+            Response.Cookies.Append("basket", stringProduct);
+
+            return RedirectToAction("ShowBasket");
+
+        }
+        public IActionResult Decrease(int?id)
+        {
+            if (id == null) return BadRequest();
+            var product = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.ProductImages)
+                .FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            List<BasketProductVM> list = new();
+            var basket = Request.Cookies["basket"];
+
+            list = JsonConvert.DeserializeObject<List<BasketProductVM>>(basket);
+
+            var existProductInBasket = list.Find(p => p.Id == id);
+            if (existProductInBasket is null)
+            {
+                BasketProductVM basketProductVM = new()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price,
+                    CategoryName = product.Category.Name,
+                    MainImageUrl = product.ProductImages.Any(p => p.IsMain) ?
+                     product.ProductImages.FirstOrDefault(p => p.IsMain).ImageUrl :
+                     product.ProductImages.FirstOrDefault().ImageUrl,
+                    BasketCount = 1
+                };
+                list.Add(basketProductVM);
+            }
+            else
+            {
+                existProductInBasket.BasketCount--;
+                if (existProductInBasket.BasketCount==0)
+                {
+                    var existProduct = list.FirstOrDefault(p => p.Id == id);
+                    list.Remove(existProduct);
+                }
+            }
+
+            var stringProduct = JsonConvert.SerializeObject(list);
+            Response.Cookies.Append("basket", stringProduct);
+
+            return RedirectToAction("ShowBasket");
+        }
+        public IActionResult Remove(int?id)
+        {
+            var stringData = Request.Cookies["basket"];
+            List<BasketProductVM> list=new();
+            
+                list = JsonConvert.DeserializeObject<List<BasketProductVM>>(stringData);
+            var existProduct=list.FirstOrDefault(p=> p.Id == id);
+            list.Remove(existProduct);
+            
+                 var stringProduct = JsonConvert.SerializeObject(list);
+            Response.Cookies.Append("basket", stringProduct);
+
+
+
+            return RedirectToAction("ShowBasket");
+        }
     }
 }
