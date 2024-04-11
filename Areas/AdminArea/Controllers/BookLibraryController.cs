c65309ff65958ec00d84291cfb6fa94bd6c19509
SelmaMammedli <selma.mammedli@gmail.com>
2024-04-11 20:57:07 +0400

    12

--- Areas/AdminArea/Controllers/BookLibraryController.cs
+++ Areas/AdminArea/Controllers/BookLibraryController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiorello.Areas.AdminArea.Controllers
 {
+    [Area("AdminArea")]
     public class BookLibraryController : Controller
     {
+        private readonly AppDbContext _context;
+        public BookLibraryController(AppDbContext context)
+        {
+            _context = context;
+            
+        }
         public IActionResult Index()
         {
             return View();
