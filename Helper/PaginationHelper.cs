defa988eb3e1aa64fe3296463d2f28106e4678f4
SelmaMammedli <selma.mammedli@gmail.com>
2024-09-07 16:48:09 +0400

    refactor pagination

--- Helper/PaginationHelper.cs
+++ Helper/PaginationHelper.cs
@@ -16,5 +16,7 @@
         public int CurrentPage { get; set; }
         public int TotalPage { get; set; }
         public IEnumerable<T> Items { get; set; }
+        public bool HasNext => CurrentPage < TotalPage;
+        public bool HasPrevious => CurrentPage > 1;
     }
 }
